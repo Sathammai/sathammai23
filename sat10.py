4fdab928fd50268b663eda1f73176271571d0d81
Sathammai <53930060+Sathammai@users.noreply.github.com>
2019-08-12 16:33:02 +0530

    Update sat10.py

--- sat10.py
+++ sat10.py
@@ -6,9 +6,9 @@ s2=s[1]
 count=0
 i=0
 while(i<len(s1) and count<2):
-    if(s1[i]!=s2[i]):
-        count+=1
-    i+=1
+ if(s1[i]!=s2[i]):
+ count+=1
+  i+=1
 if(count==1 or count==0):
     print("yes")
 else:
